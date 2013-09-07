e1ff9a64430921b0e3856a5d41c993a0ffdc71de
xiphias <xiph@freemail.hu>
2013-09-07 13:36:49 +0200

    Update package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['mongo-livedata']);
+  api.use(['mongo-livedata', 'angularjs']);
  	
   api.add_files('angular-ui.js', 'client');
   api.add_files('angular-ui.css', 'client');
